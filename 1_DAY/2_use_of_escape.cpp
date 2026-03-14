d61376c58b0d85d93b606034744b35466afbf152
Kundan Prasad <kkpp70610@gmail.com>
2026-03-14 22:59:47 +0530

    Update and rename example.cpp to 2_use_of_escape.cpp

---
+++ 1_DAY/2_use_of_escape.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World\n ";
+    
+    cout<<"Hello to Everyone\n ";
+    
+    cout<<"Hello World\n Hello to Everyone\n ";
+    
+    cout<<"Hello World "<<endl;
+    
+    cout<<"Hello to Everyone "<<endl;
+    
+    cout<<"Hello World"<<endl<< "Hello to Everyone "<<endl;
+    cout<<"nn\n\nnn\n";
+    cout<<"nn/n/nnn/n";
+}
