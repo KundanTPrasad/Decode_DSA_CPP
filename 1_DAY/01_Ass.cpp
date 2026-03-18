be88bb078818a541bedb647d8f482dd797115b32
Kundan Prasad <kkpp70610@gmail.com>
2026-03-18 22:24:51 +0530

    Add files via upload

---
+++ 1_DAY/01_Ass.cpp
@@ -0,0 +1,143 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // cout<<"         Q1           \n";
+    cout<<" Q1 Determination Of Greatest Integer"<<endl;
+    // int a,b;
+    // cout<<"Enter 1st Integer = ";
+    // cin>>a;
+    // cout<<"Enter 2nd Integer = ";
+    // cin>>b;
+    // if(a>b) cout<<a<<" is Greatest";
+    
+    // if(a<b) cout<<b<<" is Greatest";
+    // if(a==b) cout<<"Both are equal \n";
+
+    cout<<" \n \n \n Q2  comparision of area and circumference of circle \n";
+    // double r; // radius of circle
+    // cout<<"Enter radius of circle = ";
+    // cin>>r;
+    // double area = 3.14 * r * r;
+    // double circumference = 2 * 3.14 * r;
+    // cout<<"Area of circle = "<<area<<endl;
+    // cout<<"Circumference of circle = "<<circumference<<endl;
+    // if(area>circumference) cout<<"Area is greater than circumference";
+    // else cout<<"Circumference is greater than area";
+
+    // cout<<"\nDisclaimer: only magnitude are compared, not the units. \n \n \n ";
+
+    cout<<"Q3 Leap Year or Not\n";
+    // int c;
+    // cout<<"Enter Year = ";
+    // cin>>c;
+    // if(c%4==0) cout<<"Leap Year"; 
+    // else cout<<"Not leap Year"; 
+
+    cout<<"\n\nQ4 Rectangle Area and Perimeter Relation only Magnitude\n";
+    // int length, breadth;
+    // cout<<"Enter Length of Rectangle = ";
+    // cin>>length;
+    // cout<<"Enter Breadth of Rectangle = ";
+    // cin>>breadth;
+    // int area_1, Perimeter;
+    // area_1 = length * breadth;
+    // Perimeter = 2*(length + breadth);
+    // if(area_1>Perimeter) cout<<"Area of Rectangle is greater than it's Perimeter";
+    // else cout<<"Perimeter of Rectangle is greater than it's area";
+
+    cout<<"\n\nQ5 Check Types of Triangle\n";     
+//     int a1, a2, a3;
+//     cout<<"Enter 1st side of Triangle = ";
+//     cin>>a1;
+//     cout<<"Enter 2nd side of Triangle = ";
+//     cin>>a2;
+//     cout<<"Enter 3rd side of Triangle = ";
+//     cin>>a3;
+
+//    if(a1==a2){
+//         if(a2 == a3 ) cout<<"Triangle is Equilateral";
+//         else cout<<"Triangle is Isosceles";
+//    }
+//    else{  //a1 is not equal to a2 a1 != a2
+//         if (a2 == a3) cout<<"Triangle is Isosceles";
+//         else cout<<"Triangle is Scalene";
+//    }
+    cout<<"\n\nQ6 student scoring the least marks\n";
+    // int student1,student2,student3;
+    // cout<<"Enter Student 1 Marks = ";
+    // cin>>student1;
+    // cout<<"Enter Student 2 Marks = ";
+    // cin>>student2;
+    // cout<<"Enter Student 3 Marks = ";
+    // cin>>student3;
+
+    // if(student1>student2){
+    //     if(student2>student3) cout<<"Student 3 has least marks"; //s1>s2>s3
+    //     else cout<<"Student 2 has least marks"; //s1>s2<s3
+    // }
+    // else { //student1<student2                                                                   // case 1
+    //     if(student2<student3) cout<<"Student 1 has least marks"; //s1<s2<s3
+    //     else{ //s1<s2
+    //         if(student1>student3) cout<<"Student 3 has least marks"; //s1<s2 and s1>s3 => s3<s1<s2
+    //         else cout<<"Student 1 has least marks"; //s1<s2  and s1<s3  => s3<s1<s2 
+    //     }
+    // }
+    // if(student1<student2 && student1>student3)cout<<"Student 3 has least marks";
+    // else if (student1>student2 && student2<student3) cout<<"Student 2 has least marks";   //case 2
+    // else cout<<"Student 1 has least marks";
+
+    cout<<"\n\nQ7 Finding Position Of Co-ordinate\n";
+    // int x,y;
+    // cout<<"Enter x co-ordinate = ";
+    // cin>>x;
+    // cout<<"Enter y co-ordinate = ";
+    // cin>>y;
+  
+    // if (x>0){
+    //     if(y>0) cout<<"In Ist Quadrant";
+    //     else if(y==0) cout<<"On X-axis";
+    //     else cout<<"In IVth Quadrant";   
+    // }
+    // else if(x==0) {
+    //     if(y==0)cout<<"On Origin";
+    //     else cout<<"On Y-axis";
+    // }
+    // else { //x<0
+    //     if(y>0) cout<<"In II nd Quadrant";
+    //     else if (y==0) cout<<"On X-axis";
+    //     else cout<<"In III rd Quadrant";
+    // } 
+
+    cout<<"\n\nQ8 Check given three co-ordinate lies on staright line\n";
+    // float x1,x2,x3,y1,y2,y3;
+    // cout<<"Enter x1 = ";
+    // cin>>x1;
+    // cout<<"Enter y1 = ";
+    // cin>>y1;
+    //  cout<<"Enter x2 = ";
+    // cin>>x2;
+    // cout<<"Enter y2 = ";                       // use area of triangle to be zero when three points are collinear.
+    // cin>>y2;
+    //  cout<<"Enter x3 = ";
+    // cin>>x3;
+    // cout<<"Enter y2 = ";
+    // cin>>y2;
+    // float m1,m2;
+    // m1=(y2-y1)/(x2-x1);
+    // m2=(y3-y2)/(x3-x2);
+    // if(m1=m2) cout<<"All three points lies on a straight line";
+    // else cout<<"All three points not lies on a straight line";
+
+     cout<<"\n\nQ9 check whether it is the alphabet, digit or special character.\n";
+    //  char ch;
+    //  cout<<"Enter Alphabet / digit / special character, I will Guess = ";
+    //  cin>>ch;
+    //  int a;
+    //  a=(int)ch;
+    //  if((a>=65 && a<=90) || (a>=97 && a<=122) ) cout<<"Alphabet";
+    //  else if(a>=48 && a<=57) cout<<"Digit";
+    //  else if(a>=33 && a<=57) cout<<"Special character";
+    //  else cout<<"not Belongs to any set";
+
+
+}
